0948950b1cf71423b3eaf6c020ff1f4f3eedcfcb
mehrdadanvar <mehrdad.anvar@gmail.com>
2023-09-24 23:37:17 -0700

    updated pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,42 @@
 <template>
   <div>
-    <p>
-      "Where Artistry Meets History: Welcome to Lascaux Tattoo, Where Ancient
-      Inspiration Fuels Modern Expression." "Ink That Tells Stories: Lascaux
-      Tattoo - Honoring the World's First Artists." "Unearth the Past, Ink the
-      Present: Lascaux Tattoo - Your Canvas for Ancient Inspiration." "Lascaux
-      Tattoo: Where Our Passion for Tattoos Connects with Humanity's First Art."
-      "Tattoos as Timeless as History Itself - Explore the Legacy of Lascaux at
-      Our Studio." "Bridging Millennia through Art: Lascaux Tattoo - Where
-      Tradition Meets Innovation." "From the Caves to Your Skin: Lascaux Tattoo
-      - Capturing the Spirit of Ancient Creativity." "Inspired by Lascaux:
-      Tattoos That Pay Homage to the Birthplace of Art." "" "Where Body Art
-      Meets Archaeology: Lascaux Tattoo - A Testament to Human Creativity."
-    </p>
+    <article>
+      <p>
+        "Where Artistry Meets History: Welcome to Lascaux Tattoo, Where Ancient
+        Inspiration Fuels Modern Expression."
+      </p>
+      <p>
+        "Ink That Tells Stories: Lascaux Tattoo - Honoring the World's First
+        Artists."
+      </p>
+      <p>
+        "Unearth the Past, Ink the Present: Lascaux Tattoo - Your Canvas for
+        Ancient Inspiration."
+      </p>
+      <p>
+        "Lascaux Tattoo: Where Our Passion for Tattoos Connects with Humanity's
+        First Art."
+      </p>
+      <p>
+        "Tattoos as Timeless as History Itself - Explore the Legacy of Lascaux
+        at Our Studio."
+      </p>
+      <p>
+        "Bridging Millennia through Art: Lascaux Tattoo - Where Tradition Meets
+        Innovation."
+      </p>
+      <p>
+        "From the Caves to Your Skin: Lascaux Tattoo - Capturing the Spirit of
+        Ancient Creativity."
+      </p>
+      <p>
+        "Inspired by Lascaux: Tattoos That Pay Homage to the Birthplace of Art."
+      </p>
+      <p>
+        "Where Body Art Meets Archaeology: Lascaux Tattoo - A Testament to Human
+        Creativity."
+      </p>
+    </article>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-orange-500 h-screen pt-12">
+  <div class="bg-amber-900 h-screen pt-12">
     <IndexHero />
     <section class="container mx-auto">
       <h2 class="text-white text-5xl">Comming Soon!</h2>
